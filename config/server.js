a789598a3d25acf84cca56f4a6a7218cb3e09296
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2021-06-30 21:54:18 +0100

    create todo and add CRUD operations

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cae59019ba0b7214357112672435c809'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6171c54e-aee0-4e46-bffc-22966c5e0246'
+};
